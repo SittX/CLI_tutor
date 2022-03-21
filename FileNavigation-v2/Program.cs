ea41e8a6e225545a7e5f6b995f6c79b0787cea97
Kyaw Sitt Thway <kst@gmail.com>
2022-03-21 16:40:56 +0630

    Removed unused File and directory

---
+++ FileNavigation-v2/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+
+
+
+ void CheckOS()
+{
+    if (OperatingSystem.IsMacOS() && OperatingSystem.IsLinux())
+    {
+        System.Console.WriteLine("Sorry our app is not currently working in MacOS and Linux distributions.");
+    }
+    else if (OperatingSystem.IsWindows())
+    {
+        Console.WriteLine("Window");
+    }
+    else
+    {
+        System.Console.WriteLine("Can't detect the OS. Please try again.");
+    }
+}
---
+++ Utility/UserDirectory.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilitiesLibrary
+{
+    internal class UserDirectory
+    {
+        public static string GetUserSID(string userName)
+        {
+            try
+            {
+                NTAccount f = new NTAccount(userName);
+                SecurityIdentifier s = (SecurityIdentifier)f.Translate(typeof(SecurityIdentifier));
+                return s.ToString();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static string GetUserProfilePath(string userName, string userSID = null)
+        {
+            try
+            {
+                if (userSID == null)
+                {
+                    userSID = GetUserSID(userName);
+                }
+
+                var keyPath = @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\" + userSID;
+
+                var key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(keyPath);
+                if (key == null)
+                {
+                    //handle error
+                    return null;
+                }
+
+                var profilePath = key.GetValue("ProfileImagePath") as string;
+
+                return profilePath;
+            }
+            catch
+            {
+                //handle exception
+                return null;
+            }
+        }
+    }
+}
